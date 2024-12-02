8503a41fbee9148d62c73126c0ac4d22a6f35c0d
SREESANKAR-P <sreesankarputhumana@gmail.com>
2024-12-02 10:37:24 +0530

    changes in viteConfig

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig(() => {
-  const API_URL = import.meta.env.REACT_APP_API_URLL || "http://localhost:5000";
+  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
   return {
     server: {
       proxy: {
         "/api": {
           target: API_URL,
           secure: false,
+          changeOrigin: true,
         },
       },
     },
