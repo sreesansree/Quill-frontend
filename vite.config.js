e966b000598b7b6eb8827baf7ff93680042b7511
SREESANKAR-P <sreesankarputhumana@gmail.com>
2024-12-02 10:42:13 +0530

    undo backend api

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig(() => {
-  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
-  return {
-    server: {
-      proxy: {
-        "/api": {
-          target: API_URL,
-          secure: false,
-          changeOrigin: true,
-        },
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        secure: false,
       },
     },
-    plugins: [react()],
-  };
+  },
+  plugins: [react()],
 });
